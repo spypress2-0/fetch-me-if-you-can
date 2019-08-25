ad9df9366fa677ea2988d063e6c0682ea62e41e7
Michelle Herrera <mesherrera@aol.com>
2019-08-25 11:47:27 -0400

    Added express.json() and cookieParser to the connection function body. Resolved object blob issues observed in the eventListener in Connection.jsx.
    Co-authored-by: Anthony Toreson anthony.toreson@gmail.com
    Co-authored-by: Mark Shin edu.markshin@gmail.com
    Co-authored-by: Ruth Anam ruthbaanam@gmail.com

--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const app = express();
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const path = require("path");
 
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 2000 });
 
-// app.use(express.json());
-wss.on("connection", function connection(ws) {
-  // app.use(cookieParser());
+wss.on("connection", function connection(ws) { 
+  app.use(express.json());
+  app.use(cookieParser());
   app.use("*", (req, res, next) => {
-    console.log(req.body);
+    // const data = {
+    //   header: null,
+    //   cookies: null,
+    //   body: null, 
+    // }
+    console.log('server.js file', req.body);
+    // if (!res.headersSent) data.header = 'Headers not sent';
+    // if (!req.cookies) data.cookies = 'There are no cookies';
+    // if (req.body) data.body = req.body;
     ws.send(JSON.stringify(req.body));
-    next();
+    res.send();
   });
 });
 
