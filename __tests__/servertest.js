09e57ae8117dd3490c976b229f745e162a881653
Paul Rhee <rhee@Pauls-MacBook-Pro.local>
2019-08-28 21:03:04 -0400

    added testing files and basic setups

--- __tests__/servertest.js
+++ __tests__/servertest.js
@@ -1,4 +1,7 @@
 // require supertest?
+const request = require('supertest');
+
+const server = 'http://localhost:3000';
 
 
 // basic route testing
@@ -7,56 +10,27 @@
 // 2. check if websocket connection has been made (research how to test websocket connections)
 // 3. test on different Req.methods (e.g. POST, PUT, PATCH, DELETE, etc.)
 // 4. check if the data format sent to client is in JSON format
-// 5.
-// 6.
-// 7.
-// 8.
-
 
 
 // reference from testing unit:
 // https://www.npmjs.com/package/supertest
 
-describe('Route integration', () => {
-  describe('/', () => {
+describe('Basic routing on initial connection request from client', () => {
+  describe('/prod', () => {
     describe('GET', () => {
-      // Note that we return the evaluation of `request` here! It evaluates to
-      // a promise, so Jest knows not to say this test passes until that
-      // promise resolves. See https://jestjs.io/docs/en/asynchronous
       it('responds with 200 status and text/html content type', () => {
         return request(server)
-          .get('/')
+          .get('/prod')
           .expect('Content-Type', /text\/html/)
           .expect(200);
       });
     });
-  });
-
-  describe('/markets', () => {
-    describe('GET', () => {
-      it('responds with 200 status and application/json content type', () => {
+    describe('POST', () => {
+      xit('responds with 200 status and text/html content type', () => {
         return request(server)
-          .get('/markets')
-          .set('Accept', 'application/json')
+          .post('/prod')
           .expect('Content-Type', /json/)
           .expect(200);
-      });
-
-      // For this test, you'll need to inspect the body of the response and
-      // ensure it contains the markets list. Check the markets.dev.json file
-      // in the dev database to get an idea of what shape you're expecting.
-      xit('markets from "DB" jso n are in body of response', () => {
-      });
-    });
-
-    describe('PUT', () => {
-      xit('responds with 200 status and application/json content type', () => {
-      });
-
-      xit('responds with the updated market list', () => {
-      });
-
-      xit('responds to invalid request with 400 status and error message in body', () => {
       });
     });
   });
