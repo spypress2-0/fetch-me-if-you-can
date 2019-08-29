fcb87d36d6a86279342b0372593fd77f5c1ab6b0
xak200 <xak200@nyu.edu>
2019-08-29 17:03:12 -0400

    fake charts

--- client/components/Analytics.jsx
+++ client/components/Analytics.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux"; //Will need to use mapStateToProps & mapDispatchToProps
 
-const Analytics = props => {
-    return (
-        <div>HEY</div>
-    )
-};
+import Chart from "chart.js";
 
-export default Analytics;
+const mapStateToProps = store => ({});
+
+//Initialize method for props to use;
+const mapDispatchToProps = dispatch => ({});
+
+class Analytics extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        const ctx = document.querySelector("#myChart");
+        const myLineChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ["404", "200", "500", "300"],
+                datasets: [{
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9"],
+                    data: [2478, 5267, 734, 784]
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Server Activity'
+                }
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div><canvas id="myChart" width="200" height="200"></canvas></div>
+            </div>
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Analytics);
